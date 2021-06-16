47e75a09aa13fba809809030320bc6ffc4e5064e
Alistair Evans <alistair.evans@enclave.io>
2021-06-16 11:54:16 +0100

    Run enclave binary once before sudo-ing

--- external/spawn-linux.sh
+++ external/spawn-linux.sh
@@ -7,6 +7,10 @@ echo -e "Spawning Enclave background process"
 sudo chown runner: $ENCLAVE_BINARY
 sudo chmod 755 $ENCLAVE_BINARY
 
+# Run without sudo once so that the extract directory 
+# doesn't get created with root ownership
+$ENCLAVE_BINARY version
+
 # Launch enclave, daemonised.
 sudo -E $ENCLAVE_BINARY run </dev/null &>/dev/null &
 
