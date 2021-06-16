b33d4f0d284feca7f5c40176767726d316e5e2df
Alistair Evans <alistair.evans@enclave.io>
2021-06-16 10:28:24 +0100

    Tweaking spawn script; add nohup

--- external/spawn-linux.sh
+++ external/spawn-linux.sh
@@ -5,4 +5,4 @@ set -euo pipefail
 echo -e "Spawning Enclave background process"
 
 # Launch enclave, daemonised.
-sudo -E enclave run </dev/null &> /dev/null &
+nohup sudo -E enclave run </dev/null &>/dev/null &
