bfc9eb3582ad93722cc266bcab394986d2fc1c13
Alistair Evans <alistair.evans@enclave.io>
2021-06-16 10:43:18 +0100

    Just run Enclave, see what's wrong

--- external/spawn-linux.sh
+++ external/spawn-linux.sh
@@ -5,6 +5,6 @@ set -euo pipefail
 echo -e "Spawning Enclave background process"
 
 # Launch enclave, daemonised.
-sudo nohup -E enclave run </dev/null &>/dev/null &
+sudo -E enclave run
 
 echo -e "Enclave background process started"
