033ed302470390c6f3c436efaa25f50764565026
Alistair Evans <alistair.evans@enclave.io>
2021-06-16 11:04:35 +0100

    Change chmod options

--- external/spawn-linux.sh
+++ external/spawn-linux.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 echo -e "Spawning Enclave background process"
 
-chmod +x $ENCLAVE_BINARY
+chmod 755 $ENCLAVE_BINARY
 
 # Launch enclave, daemonised.
 sudo -E $ENCLAVE_BINARY run </dev/null &>/dev/null &
