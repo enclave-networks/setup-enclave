5259a4b331292b41d4f5135fbe1c4a5315c32459
Alistair Evans <alistair.evans@enclave.io>
2021-10-18 15:11:10 +0100

    Use a tmp file for dumping logs

--- external/spawn-linux.sh
+++ external/spawn-linux.sh
@@ -9,8 +9,8 @@ sudo chmod 755 $ENCLAVE_BINARY
 
 export DOTNET_BUNDLE_EXTRACT_BASE_DIR=$RUNNER_TEMP/enclave-daemon-extract
 # Launch enclave, daemonised.
-sudo -E $ENCLAVE_BINARY run </dev/null &>/var/log/enclave-launch &
+sudo -E $ENCLAVE_BINARY run </dev/null &>$RUNNER_TEMP/enclave-launch &
 
-cat /var/log/enclave-launch
+cat $RUNNER_TEMP/enclave-launch
 
 echo -e "Enclave background process started"
