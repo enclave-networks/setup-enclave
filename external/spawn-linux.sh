9286a672bd83240a371455475ddd1cbd3e59d8ac
Alistair Evans <alistair.evans@enclave.io>
2021-10-18 15:13:01 +0100

    Just try running the process

--- external/spawn-linux.sh
+++ external/spawn-linux.sh
@@ -9,8 +9,6 @@ sudo chmod 755 $ENCLAVE_BINARY
 
 export DOTNET_BUNDLE_EXTRACT_BASE_DIR=$RUNNER_TEMP/enclave-daemon-extract
 # Launch enclave, daemonised.
-sudo -E $ENCLAVE_BINARY run </dev/null &>$RUNNER_TEMP/enclave-launch &
-
-cat $RUNNER_TEMP/enclave-launch
+sudo -E $ENCLAVE_BINARY run
 
 echo -e "Enclave background process started"
